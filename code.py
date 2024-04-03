91e1bc418855b58e557c5e79285c798cbb4c1bf2
Jeremy Muller <jeremymuller92@gmail.com>
2024-04-03 10:01:03 +0200

    avancement du code: besoin de résoudre le scraping du tableau

--- code.py
+++ code.py
@@ -7,20 +7,27 @@ reponse = requests.get(url)
 page = reponse.content
 soup = BeautifulSoup(page, "html.parser")
 
+upcs = soup.find("td", {"data-title": "UPC"})
+
 titres = soup.find_all("li", class_="active")
 titre_textes = []
 for titre in titres:
     titre_textes.append(titre.string)
+"#recupération du titre du livre"
 
 descriptions = soup.find_all("p")[3]
 description_textes = []
-
 for description in descriptions:
     description_textes.append(description.string)
+"#récupération de la description du livre"
+prix_ht = soup.find()
 
-en_tete = ["url", "titre", "description"]
+"#récupération du prix hors-taxe du livre"
+en_tete = ["product_page_url", "upc", "title", "price_including_tax", "price_excluding_tax", "number_avaible", "product_description", "category", "review_rating", "image_url"]
+"#ici upc signifie Universal Product Code"
+
 with open("data.csv", "w") as csv_file:
     writer = csv.writer(csv_file, delimiter=",")
     writer.writerow(en_tete)
     for titre, description in zip(titre_textes, description_textes):
-        writer.writerow([titre, description])
+        writer.writerow([url, upcs, titre, description])

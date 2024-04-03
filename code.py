c72d52986e83947bc8f932c68616aee27d007e91
Jeremy Muller <jeremymuller92@gmail.com>
2024-04-03 10:34:51 +0200

    correction commentaires

--- code.py
+++ code.py
@@ -7,24 +7,25 @@ reponse = requests.get(url)
 page = reponse.content
 soup = BeautifulSoup(page, "html.parser")
 
-upcs = soup.find("td", {"data-title": "UPC"})
+upcs = soup.find_all("td", {"data-title": "UPC"})
+print(upcs)
 
 titres = soup.find_all("li", class_="active")
 titre_textes = []
 for titre in titres:
     titre_textes.append(titre.string)
-"#recupération du titre du livre"
+# recupération du titre du livre
 
 descriptions = soup.find_all("p")[3]
 description_textes = []
 for description in descriptions:
     description_textes.append(description.string)
-"#récupération de la description du livre"
+# récupération de la description du livre
 prix_ht = soup.find()
 
-"#récupération du prix hors-taxe du livre"
+# récupération du prix hors-taxe du livre
 en_tete = ["product_page_url", "upc", "title", "price_including_tax", "price_excluding_tax", "number_avaible", "product_description", "category", "review_rating", "image_url"]
-"#ici upc signifie Universal Product Code"
+# ici upc signifie Universal Product Code
 
 with open("data.csv", "w") as csv_file:
     writer = csv.writer(csv_file, delimiter=",")

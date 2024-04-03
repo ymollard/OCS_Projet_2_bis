3d48ac9d579cb0963a10c16271983aab6521020e
Jeremy Muller <jeremymuller92@gmail.com>
2024-04-03 12:09:17 +0200

    extraction des données 8/10, code mis en page

--- code.py
+++ code.py
@@ -7,8 +7,29 @@ reponse = requests.get(url)
 page = reponse.content
 soup = BeautifulSoup(page, "html.parser")
 
-upcs = soup.find_all("td", {"data-title": "UPC"})
-print(upcs)
+upcs = soup.find_all("td")[0]
+upcs_liste = []
+for upc in upcs:
+    upcs_liste.append(upc)
+# recupération de l'upc du livre
+
+prix_s_ttc = soup.find_all("td")[2]
+prix_ttc_liste = []
+for prix_ttc in prix_s_ttc:
+    prix_ttc_liste.append(prix_ttc)
+# recupération du prix ttc du livre
+
+prix_s_ht = soup.find_all("td")[3]
+prix_ht_liste = []
+for prix_ht in prix_s_ht:
+    prix_ht_liste.append(prix_ht)
+# recupération du prix ht du livre
+
+stocks = soup.find_all("td")[5]
+stocks_liste = []
+for stock in stocks:
+    stocks_liste.append(stock)
+# recupération du stock du livre
 
 titres = soup.find_all("li", class_="active")
 titre_textes = []
@@ -21,14 +42,24 @@ description_textes = []
 for description in descriptions:
     description_textes.append(description.string)
 # récupération de la description du livre
-prix_ht = soup.find()
 
-# récupération du prix hors-taxe du livre
+categories = soup.find_all("a")[3]
+categories_liste = []
+for categorie in categories:
+    categories_liste.append(categorie.string)
+# recupération de la catégorie du livre
+
+avis = soup.find
+# recupération des avis du livre
+
+# recupération de l'url de l'image du livre
+
+
 en_tete = ["product_page_url", "upc", "title", "price_including_tax", "price_excluding_tax", "number_avaible", "product_description", "category", "review_rating", "image_url"]
 # ici upc signifie Universal Product Code
 
 with open("data.csv", "w") as csv_file:
     writer = csv.writer(csv_file, delimiter=",")
     writer.writerow(en_tete)
-    for titre, description in zip(titre_textes, description_textes):
-        writer.writerow([url, upcs, titre, description])
+    for upc, titre, prix_ttc, prix_ht, stock, description, categorie in zip(upcs_liste, titre_textes, prix_ttc_liste, prix_ht_liste, stocks_liste, description_textes, categories_liste):
+        writer.writerow([url, upc, titre, prix_ttc, prix_ht, stock, description, categorie])

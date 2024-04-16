8fd51a1fdb284f3b9291106662e41430e4d7809f
Jeremy Muller <jeremymuller92@gmail.com>
2024-04-16 18:32:12 +0200

    code épuré, pagination toujours pas fonctionnelle

--- code.py
+++ code.py
@@ -2,108 +2,94 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-# obtention du code de la page de la catégorie:
-url_categorie = "https://books.toscrape.com/catalogue/category/books/fantasy_19/index.html"
-reponse = requests.get(url_categorie)
-page = reponse.text
-soup = BeautifulSoup(page, "html.parser")
+# création d'un premier objet soup
+url_site = "https://books.toscrape.com/index.html"
+reponse_site = requests.get(url_site)
+page_site = reponse_site.text
+soup_site = BeautifulSoup(page_site, "html.parser")
+# création d'une liste de lien par catégories
+url_categorie_liste = []
+url_categories = soup_site.find_all("a")[3:53]
+for url_categorie in url_categories:
+    url_categorie = "https://books.toscrape.com/" + url_categorie["href"]
+    url_categorie_liste.append(url_categorie)
 
-# récupération de l'url des différents livres de la page
-url_livres = soup.find_all("h3")
-url_livres_liste = []
-for h3 in url_livres:
-    a = h3.find("a")
-    url_livre = a["href"]
-    url_livre = str(url_livre)
-    url_livres_absolue = "https://books.toscrape.com/catalogue/" + url_livre[9:]
-    url_livres_liste.append(url_livres_absolue)
+# récupération de tous les liens des livres d'une catégorie
+for url_categorie in url_categorie_liste:
+    reponse = requests.get(url_categorie)
+    page = reponse.text
+    soup = BeautifulSoup(page, "html.parser")
 
-# création d'une boucle pour parcourir les différents livres
-for url_livres_absolue in url_livres_liste:
-    reponse2 = requests.get(url_livres_absolue)
-    page2 = reponse2.content
-    soup2 = BeautifulSoup(page2, "html.parser")
+    # récupération de l'url des différents livres de la page
+    url_livres_liste = []
+    url_livres = soup.find_all("h3")
+    for h3 in url_livres:
+        a = h3.find("a")
+        url_livre = a["href"]
+        url_livre = str(url_livre)
+        url_livres_absolue = "https://books.toscrape.com/catalogue/" + url_livre[9:]
+        url_livres_liste.append(url_livres_absolue)
 
-# recupération de l'upc du livre
-    upcs = soup2.find_all("td")[0]
     upcs_liste = []
-    for upc in upcs:
-        upcs_liste.append(upc.text)
-
-# recupération du prix ttc du livre
-    prix_s_ttc = soup2.find_all("td")[2]
     prix_ttc_liste = []
-    for prix_ttc in prix_s_ttc:
-        prix_ttc_liste.append(prix_ttc)
-
-# recupération du prix ht du livre
-    prix_s_ht = soup2.find_all("td")[3]
     prix_ht_liste = []
-    for prix_ht in prix_s_ht:
-        prix_ht_liste.append(prix_ht)
-
-# recupération du stock du livre
-    stocks = soup2.find_all("td")[5]
     stocks_liste = []
-    for stock in stocks:
-        stocks_liste.append(stock)
-
-# recupération du titre du livre
-    titres = soup2.find_all("h1")
     titre_textes = []
-    for titre in titres:
-        titre_textes.append(titre.string)
-
-# récupération de la description du livre
-    descriptions = soup2.find_all("p")[3]
     description_textes = []
-    for description in descriptions:
-        description_textes.append(description.string)
-
-
-# recupération de la catégorie du livre
-    categories = soup2.find_all("a")[3]
     categories_liste = []
-    for categorie in categories:
-        categories_liste.append(categorie)
-
-# récupération du "review rating" du livre
-    aviss = soup2.find_all("p", class_="star-rating")
-    if aviss:
-        premieravis = aviss[0]
-        if "Three" in premieravis["class"]:
-            avis = "Ce livre est noté 3/5"
-        elif "One" in premieravis["class"]:
-            avis = "Ce livre est noté 1/5"
-        elif "Two" in premieravis["class"]:
-            avis = "Ce livre est noté 2/5"
-        elif "Four" in premieravis["class"]:
-            avis = "Ce livre est noté 4/5"
-        elif "Five" in premieravis["class"]:
-            avis = "Ce livre est noté 5/5"
-    else:
-        avis = "Ce livre n'est pas noté"
     avis_liste = []
-    for i in (len(url_livres_liste)):
-        avis_liste.append(aviss)
-    print(avis_liste)
-
-
-# recupération de l'url de l'image du livre
-    url_img_relatif = soup2.img['src']
-    url_img_relatif = str(url_img_relatif)
-    url_img_absolues = "https://books.toscrape.com/" + url_img_relatif[6:]
     url_img_liste = []
-    for i in range(len(url_livres_liste)):
-        url_img_liste.append(url_img_absolues)
+    # création d'une boucle pour parcourir les différents livres
+    for url_livres_absolue in url_livres_liste:
+        reponse2 = requests.get(url_livres_absolue)
+        page2 = reponse2.content
+        soup2 = BeautifulSoup(page2, "html.parser")
 
-en_tete = ["product_page_url", "upc", "title", "price_including_tax", "price_excluding_tax", "number_avaible", "product_description", "category", "review_rating", "image_url"]
-# ici upc signifie Universal Product Code
+    # recupération de l'upc du livre
+        upc = soup2.find_all("td")[0].text
+        upcs_liste.append(upc)
+    # recupération du prix ttc du livre
+        prix_ttc = soup2.find_all("td")[2].text
+        prix_ttc_liste.append(prix_ttc)
+    # recupération du prix ht du livre
+        prix_ht = soup2.find_all("td")[3].text
+        prix_ht_liste.append(prix_ht)
+    # recupération du stock du livre
+        stock = soup2.find_all("td")[5].text
+        stocks_liste.append(stock)
+    # recupération du titre du livre
+        titre = soup2.find("h1").text
+        titre_textes.append(titre)
+    # récupération de la description du livre
+        description = soup2.find_all("p")[3].text
+        description_textes.append(description)
+    # recupération de la catégorie du livre
+        categorie = soup2.find_all("a")[3].text
+        categories_liste.append(categorie)
+    # récupération du "review rating" du livre
+        avis = soup2.find("p", class_="star-rating")["class"]
+        avis_liste.append(avis)
+    # recupération de l'url de l'image du livre
+        url_img = soup2.img['src']
+        url_img = str(url_img)
+        url_img = "https://books.toscrape.com/" + url_img[6:]
+        url_img_liste.append(url_img)
 
-# création du fichier csv pour stocker les données
-with open("data.csv", "w") as csv_file:
-    writer = csv.writer(csv_file, delimiter=",")
-    writer.writerow(en_tete)
-    for url_livre, upc, titre, prix_ttc, prix_ht, stock, description, categorie, url_img in zip(url_livres_liste, upcs_liste, titre_textes, prix_ttc_liste, prix_ht_liste, stocks_liste, description_textes, categories_liste, url_img_absolu):
-        writer.writerow([url_livre, upc, titre, prix_ttc, prix_ht, stock, description, categorie, url_img_absolu])
+    # création du fichier csv pour stocker les données
+    en_tete = ["product_page_url", "upc", "title", "price_including_tax", "price_excluding_tax", "number_avaible", "product_description", "category", "review_rating", "image_url"]
+    with open("data.csv", "w") as csv_file:
+        writer = csv.writer(csv_file, delimiter=",")
+        writer.writerow(en_tete)
+        for i in range(len(url_livres_liste)):
+            url_livre = url_livres_liste[i]
+            upc = upcs_liste[i]
+            titre = titre_textes[i]
+            prix_ttc = prix_ttc_liste[i]
+            prix_ht = prix_ht_liste[i]
+            stock = stocks_liste[i]
+            description = description_textes[i]
+            categorie = categories_liste[i]
+            avis = avis_liste[i]
+            url_img = url_img_liste[i]
+            writer.writerow([url_livre, upc, titre, prix_ttc, prix_ht, stock, description, categorie, avis, url_img])
 

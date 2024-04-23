2d6f7dfff06331e1d54206f758d83a6dab5c46a5
Jeremy Muller <jeremymuller92@gmail.com>
2024-04-23 20:59:06 +0200

    Modification branche main

---
+++ README.md
@@ -0,0 +1,55 @@
+Ce fichier README a été généré le [2024-04-19] par [MULLER Jérémy].
+
+Dernière mise-à-jour le : [2024-04-19].
+
+# INFORMATIONS GENERALES
+
+## Titre du jeu de données : Programme d'extraction des prix
+ 
+## Adresse de contact : jeremymuller92@gmail.com
+ 
+# INFORMATIONS METHODOLOGIQUES
+
+## Conditions environnementales / experimentales : 
+Veuillez vous assurer de la qualité de votre connexion internet et que l'environnement de travail comprend les paquets python cités dans le fichier "requirements.txt".
+
+## Description des sources et méthodes utilisées pour collecter et générer les données :
+<Si applicable, décrire les standards, les informations de calibration, les instruments utilisés, etc.>
+
+## Méthodes de traitement des données :
+<Si applicable, décrire le traitement des données et inclure tout détail pouvant être important pour réutiliser ou reproduire les données. Commenter chaque étape.
+Par exemple, inclure les méthodes de nettoyage et d’analyse ; les codes et/ou algorithmes ; les procédés d’anonymisation ou de pseudonymisation pour les données sensibles concernant les humains ou des espèces menacées>
+
+## Procédures d’assurance-qualité appliquées sur les données :
+
+## Autres informations contextuelles :
+<Toute information que vous considérez importante pour évaluer la qualité du jeu de données ou pour sa réutilisation : par exemple, des informations concernant les logiciels nécessaires pour interpréter les données.
+Si applicable et non-inclus préalablement, ajouter les noms complets et les versions de tous les logiciels, de tous les paquets et de toutes les librairies nécessaires pour lire et interpréter les données *e.g.* pour compiler les scripts.>
+
+# APERCU DES DONNEES ET FICHIERS
+
+
+## Convention de nommage des fichiers :
+
+## Arborescence/plan de classement des fichiers :
+
+
+# INFORMATIONS SPECIFIQUES AUX DONNEES POUR : [NOM DU FICHIER]
+
+<Le cas échéant, reproduire cette section pour chaque dossier ou fichier.
+Les éléments se répétant peuvent être expliqués dans une section initiale commune.>
+
+<Pour les données tabulaires, fournir un dictionnaire des données/manuel de codage contenant les informations suivantes :>
+## Liste des variables/entêtes de colonne :
+
+Pour chaque nom de variable ou entête de colonne, indiquer :
+ 
+    -- le nom complet de la variable sous forme “lisible par les humains” ; 
+    -- la description de la variable ; 
+    -- unité de mesure, si applicable ; 
+    -- séparateur décimal *i.e.* virgule ou point, si applicable ; 
+    -- valeurs autorisées : liste ou plage de valeurs, ou domaine ;
+    -- format, si applicable, e.g. date>
+
+## Informations additionnelles : 
+<Toute information que vous jugez utile pour mieux comprendre le fichier>
--- code.py
+++ code.py
@@ -8,6 +8,7 @@ url_site = "https://books.toscrape.com/index.html"
 reponse_site = requests.get(url_site)
 page_site = reponse_site.text
 soup_site = BeautifulSoup(page_site, "html.parser")
+
 # création d'une liste de lien par catégories
 url_categorie_liste = []
 url_categories = soup_site.find_all("a")[3:6]
@@ -20,6 +21,7 @@ for url_categorie in url_categorie_liste:
     reponse = requests.get(url_categorie)
     page = reponse.text
     soup = BeautifulSoup(page, "html.parser")
+
     # prise en compte de la pagination:
     if soup.find("ul", class_="pager"):
         lien_next = soup.find("li", class_="next")
@@ -28,7 +30,7 @@ for url_categorie in url_categorie_liste:
             path = "/".join(url_categorie.split("/")[:-1]) + "/"
             lien_next = path + lien_next
             url_categorie_liste.append(lien_next)
-            url_categorie_liste.sort()
+
     # récupération de l'url des différents livres de la page
     url_livres_liste = []
     url_livres = soup.find_all("h3")
@@ -38,6 +40,7 @@ for url_categorie in url_categorie_liste:
         url_livre = str(url_livre)
         url_livres_absolue = "https://books.toscrape.com/catalogue/" + url_livre[9:]
         url_livres_liste.append(url_livres_absolue)
+
     # création d'un objet soup par livre et récupération de la catégorie du livre
     categories_liste = []
     for url_livres_absolue in url_livres_liste:
@@ -46,6 +49,7 @@ for url_categorie in url_categorie_liste:
         soup2 = BeautifulSoup(page2, "html.parser")
         categorie = soup2.find_all("a")[3].text
         categories_liste.append(categorie)
+
     # Création des dossiers utlisés par notre script
     dossier_img = str(categorie) + "_img"
     if not os.path.exists(dossier_img):
@@ -53,6 +57,8 @@ for url_categorie in url_categorie_liste:
     dossier_data = str(categorie) + "_data"
     if not os.path.exists(dossier_data):
         os.makedirs(dossier_data)
+
+
     # Création des listes utlisées par notre script
     upcs_liste = []
     prix_ttc_liste = []
@@ -62,34 +68,47 @@ for url_categorie in url_categorie_liste:
     description_textes = []
     avis_liste = []
     url_img_liste = []
+
+
     # création d'une boucle pour parcourir les différents livres
     for url_livres_absolue in url_livres_liste:
-    # recupération de l'upc du livre
+
+        # recupération de l'upc du livre
         upc = soup2.find_all("td")[0].text
         upcs_liste.append(upc)
-    # recupération du prix ttc du livre
+
+        # recupération du prix ttc du livre
         prix_ttc = soup2.find_all("td")[2].text
         prix_ttc_liste.append(prix_ttc)
-    # recupération du prix ht du livre
+
+        # recupération du prix ht du livre
         prix_ht = soup2.find_all("td")[3].text
         prix_ht_liste.append(prix_ht)
-    # recupération du stock du livre
+
+        # recupération du stock du livre
         stock = soup2.find_all("td")[5].text
         stocks_liste.append(stock)
-    # recupération du titre du livre
+
+        # recupération du titre du livre
         titre = soup2.find("h1").text
         titre_textes.append(titre)
-    # récupération de la description du livre
+
+        # récupération de la description du livre
         description = soup2.find_all("p")[3].text
         description_textes.append(description)
-    # récupération du "review rating" du livre
+
+        # récupération du "review rating" du livre
         avis = soup2.find("p", class_="star-rating")["class"]
         avis = str(avis)[2:13] + " " + str(avis)[17:-2]
         avis_liste.append(avis)
-    # recupération de l'url de l'image du livre
+
+        # recupération de l'url de l'image du livre
         url_img = soup2.img['src']
         url_img = str(url_img)
         url_img = "https://books.toscrape.com/" + url_img[6:]
+        url_img_liste.append(url_img)
+
+        # téléchargement de l'image
         reponse_img = requests.get(url_img)
         if reponse_img.status_code == 200:
             titre = titre.replace(":", "-")
@@ -98,7 +117,7 @@ for url_categorie in url_categorie_liste:
                 fichier_img.write(reponse_img.content)
         else:
             print("Erreur lors du téléchargement de l'image")
-        url_img_liste.append(url_img)
+
 
     # création du fichier csv pour stocker les données
     for url_categorie in url_categorie_liste:
@@ -108,14 +127,4 @@ for url_categorie in url_categorie_liste:
             writer = csv.writer(csv_file, delimiter=",")
             writer.writerow(en_tete)
             for i in range(len(url_livres_liste)):
-                url_livre = url_livres_liste[i]
-                upc = upcs_liste[i]
-                titre = titre_textes[i]
-                prix_ttc = prix_ttc_liste[i]
-                prix_ht = prix_ht_liste[i]
-                stock = stocks_liste[i]
-                description = description_textes[i]
-                categorie = categories_liste[i]
-                avis = avis_liste[i]
-                url_img = url_img_liste[i]
-                writer.writerow([url_livre, upc, titre, prix_ttc, prix_ht, stock, description, categorie, avis, url_img])
+                writer.writerow([url_livres_liste[i], upcs_liste[i], titre_textes[i], prix_ttc_liste[i], prix_ht_liste[i], stocks_liste[i], description_textes[i], categories_liste[i], avis_liste[i], url_img_liste[i]])

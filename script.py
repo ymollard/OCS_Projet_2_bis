5655d3abae4f2a073631a4ea8229d5e36b077efc
Jeremy Muller <jeremymuller92@gmail.com>
2024-05-10 10:03:23 +0200

    Maj vendredi

--- script.py
+++ script.py
@@ -42,26 +42,73 @@ def search_url(link):
             lien_next = path + lien_next
             search_url(lien_next)
 
+
 # Création d'une première boucle parcourant les catégories
 for url_categorie in url_categorie_liste:
     search_url(url_categorie)
 
-
-    # Création des dossiers utlisés par notre script
-    dossier_img = str(categorie) + "_img"
-    if not os.path.exists(dossier_img):
-        os.makedirs(dossier_img)
-    dossier_data = str(categorie) + "_data"
-    if not os.path.exists(dossier_data):
-        os.makedirs(dossier_data)
-    # Création d'un fichier csv pour sotcker les données
+    # Création d'un fichier csv pour stocker les données
     en_tete = ["product_page_url", "upc", "title", "price_including_tax", "price_excluding_tax", "number_avaible",
                "product_description", "category", "review_rating", "image_url"]
     path_data = os.path.join(dossier_data, categorie + ".csv")
     with open(path_data, "w", encoding="utf-8", newline="") as csv_file:
         writer = csv.writer(csv_file, delimiter=",")
         writer.writerow(en_tete)
-        for i in range(len(url_livres_liste)):
-            writer.writerow([url_livres_liste[i], upcs_liste[i], titre_textes[i], prix_ttc_liste[i], prix_ht_liste[i],
-                             stocks_liste[i], description_textes[i], categories_liste[i], avis_liste[i],
-                             url_img_liste[i]])
+
+    # création d'une boucle pour parcourir les différents livres
+    for url_livre in url_livre_liste:
+
+        # Définition d'un objet soup par livre
+        response_2 = requests.get(url_livre)
+        page_2 = response_2.content
+        soup_2 = BeautifulSoup(page_2, "html.parser")
+
+        # Récupération de l'upc du livre
+        upc = soup_2.find_all("td")[0].text
+
+        # recupération du prix ttc du livre
+        prix_ttc = soup_2.find_all("td")[2].text
+
+        # recupération du prix ht du livre
+        prix_ht = soup_2.find_all("td")[3].text
+
+        # recupération du stock du livre
+        stock = soup_2.find_all("td")[5].text
+
+        # recupération du titre du livre
+        titre = soup_2.find("h1").text
+
+        # Récupération de la description du livre
+        description = soup_2.find_all("p")[3].text
+
+        # Récupération de la catégorie du livre
+        categorie = soup_2.find_all("a")[3].text
+
+        # Récupération du "review rating" du livre
+        avis = soup_2.find("p", class_="star-rating")["class"]
+        avis = str(avis)[2:13] + " " + str(avis)[17:-2]
+
+        # Récupération de l'url de l'image du livre
+        url_img = soup_2.img['src']
+        url_img = str(url_img)
+        url_img = "https://books.toscrape.com/" + url_img[6:]
+
+        # Création, si besoin des dossiers utlisés par notre script
+        dossier_img = str(categorie) + "_img"
+        if not os.path.exists(dossier_img):
+            os.makedirs(dossier_img)
+        dossier_data = str(categorie) + "_data"
+        if not os.path.exists(dossier_data):
+            os.makedirs(dossier_data)
+
+        writer.writerow([url_livre, upc, prix_ttc, prix_ht, stock, titre, description, categorie, avis, url_img])
+
+        # Téléchargement de l'image du livre
+        reponse_img = requests.get(url_img)
+        if reponse_img.status_code == 200:
+            titre = titre.replace(":", "-")
+            path_img = os.path.join(dossier_img, titre + ".jpg")
+            with open(path_img, "wb") as fichier_img:
+                fichier_img.write(reponse_img.content)
+        else:
+            print("Erreur lors du téléchargement de l'image")
